c650ac8ebd2ddba3394a9ee072e1ba62f1e3fb58
Fabian De La Cruz <fahuervodelacruz@hotmail.com>
2025-01-09 10:43:18 -0600

    arreglo del boton en url public

--- src/components/BotonDescargaPDF.jsx
+++ src/components/BotonDescargaPDF.jsx
@@ -1,16 +1,17 @@
 const BotonDescargaPDF = () => {
   const handleDownload = () => {
-    const url = "public/CV.pdf"; // Ruta correcta desde public
+    // Asegúrate de que el archivo esté dentro de la carpeta 'public'
+    const url = "/CV.pdf"; // Ruta relativa al archivo estático en la carpeta public
     const link = document.createElement("a");
     link.href = url;
-    link.download = "CV.pdf"; // Nombre con el que se descargará el archivo
+    link.download = "CV.pdf"; // El nombre con el que se descargará el archivo
     link.click();
   };
 
   return (
     <button
       onClick={handleDownload}
-      className="transition-colors text-base px-6 py-1 bg-purple-950 text-w font-mono rounded-lg shadow-md hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+      className="transition-colors text-base px-6 py-1 bg-purple-950 text-white font-mono rounded-lg shadow-md hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
     >
       Descargar CV
     </button>
